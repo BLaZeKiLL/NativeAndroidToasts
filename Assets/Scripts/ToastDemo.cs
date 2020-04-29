d7b97a34e15dd685b2f2aa5dd0c9e27cf7c64a65
Devashish Lal <laldevashish@gmail.com>
2020-04-29 22:35:32 +0530

    Project Files Added

---
+++ Assets/Scripts/ToastDemo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToastDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToastManager.Show("Hello world", ToastManager.Length.LONG);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
